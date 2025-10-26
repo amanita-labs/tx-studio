403972b655343592d2746e8ae2a5d22389cc9637
Ryan Williams <ryan.williams@intersectmbo.org>
2025-10-26 21:45:36 +0000

    try to fix github pages deployment

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'out',
+  basePath: process.env.NODE_ENV === 'production' ? '/tx-studio' : '',
   images: {
     unoptimized: true,
   },
